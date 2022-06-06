8b8ce3f8a9cfa7bcfd628a3fc81eb249ee5fe9d0
artyom <pretkovartyom@gmail.com>
2022-06-06 17:15:26 +0300

    Initial commit

---
+++ src/main/java/org/temkarus0070/dsrpracticeproject/DsrPracticeProjectApplication.java
@@ -0,0 +1,13 @@
+package org.temkarus0070.dsrpracticeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsrPracticeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsrPracticeProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/temkarus0070/dsrpracticeproject/DsrPracticeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.temkarus0070.dsrpracticeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsrPracticeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
